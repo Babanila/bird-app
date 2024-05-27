c243be11a22adb9d5ce67101d45172d1dfb2e484
babanila <babanila@yahoo.com>
2024-05-27 08:43:00 +0200

    Add category selection

--- shared/src/commonMain/kotlin/BirdsViewModel.kt
+++ shared/src/commonMain/kotlin/BirdsViewModel.kt
@@ -12,8 +12,12 @@ import model.BirdImage
 
 
 data class BirdsUiState(
-    val images: List<BirdImage> = emptyList()
-)
+    val images: List<BirdImage> = emptyList(),
+    val selectedCategory: String? = null
+) {
+    val categories = images.map { it.category}.toSet()
+    val selectedImages = images.filter { it.category == selectedCategory}
+}
 
 class BirdsViewModel: ViewModel () {
     private val _uiState = MutableStateFlow<BirdsUiState>(BirdsUiState())
@@ -31,6 +35,12 @@ class BirdsViewModel: ViewModel () {
 
     override fun onCleared() {
         httpClient.close()
+    }
+
+    fun selectCategory(category: String) {
+        _uiState.update {
+            it.copy(selectedCategory = category)
+        }
     }
 
     private fun updateImages() {
